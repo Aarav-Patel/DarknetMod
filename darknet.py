41ad591352ddb88bf2bd522ce991f60d89cfdbc6
Aarav-Patel <68515150+Aarav-Patel@users.noreply.github.com>
2020-11-01 20:16:25 -0500

    Update darknet.py

--- darknet.py
+++ darknet.py
@@ -18,8 +18,9 @@ Directly viewing or returning bounding-boxed images requires scikit-image to be 
 Original *nix 2.7: https://github.com/pjreddie/darknet/blob/0f110834f4e18b30d5f101bf8f1724c34b7b83db/python/darknet.py
 Windows Python 2.7 version: https://github.com/AlexeyAB/darknet/blob/fc496d52bf22a0bb257300d3c79be9cd80e722cb/build/darknet/x64/darknet.py
 
-@author: Philip Kahn
-@date: 20180503
+@author: Philip Kahn (Original) and Aarav Patel (Modifcation: Social Distancing Detector)
+@date(Orginal): 20180503
+@data(Modifcation: Social Distancing Detector): 20201101
 """
 from ctypes import *
 import math
